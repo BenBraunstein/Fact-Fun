22e84542f4203b2614b278b0436a545532b41209
nicholasstano <nicholasstano@gmail.com>
2019-08-19 16:05:56 -0400

    Third Commit -- making progress

--- test.js
+++ test.js
@@ -8,7 +8,8 @@ fetch(
         // console.log(Object.entries(json.countries))
         let randomCountry = getRandomCountry(json)
         // console.log(randomCountry)
-        console.log(getCountryFacts(randomCountry))
+        // console.log(getCountryFacts(randomCountry))
+        renderQuestion(getCountryFacts(randomCountry), json)
     })
 
 function getRandomInteger(min, max) {
@@ -25,16 +26,53 @@ function getCountryFacts(country) {
         borderCountriesArray.push(country.country)
     })
     return {
-        countryName: country[0],
+        countryName: country[1].data.name,
         countryComparativeSize: country[1].data.geography.area.comparative,
-        countryLandBoundaries: borderCountriesArray,
         flagDescription: country[1].data.government.flag_description.description.split(";")[0],
         mostPopularReligion: country[1].data.people.religions.religion[0].name,
         religionPercent: country[1].data.people.religions.religion[0].percent,
         populationRank: country[1].data.people.population.global_rank,
+        countryLandBoundaries: borderCountriesArray,
         nationalAnthem: country[1].data.government.national_anthem.audio_url
     }
 }
 
+function renderQuestion(countryFacts, json) {
+    console.log(countryFacts)
+    let questionContainer = document.querySelector(".questionCard")
+    let buttonArray = []
+    buttonArray.push(`<button onclick='questionAnswered(this)'>${countryFacts.countryName}</button>`)
+    for (let i = 0; i < 3; i++) {
+        let randomCountryName = getRandomCountry(json)[1].data.name
+        buttonArray.push(`<button onclick='questionAnswered(this)'>${randomCountryName}</button>`)
+    }
+    buttonArray = shuffle(buttonArray)
+    questionContainer.insertAdjacentHTML("beforeend",
+        `<h1> What Country am I ?</h1 >
+        <ul>
+            <li>I am ${countryFacts.countryComparativeSize}</li>
+            <li>My Flag has ${countryFacts.flagDescription}</li>
+            <li>${countryFacts.religionPercent}% of my country is ${countryFacts.mostPopularReligion}</li>
+            <li>The population rank of this country is #${countryFacts.populationRank}</li>
+            <li>My country borders the following: ${countryFacts.countryLandBoundaries.join(", ")}</li>
+        </ul>
+        ${buttonArray.join(' ')}`)
+}
+
+function questionAnswered(button) {
+    console.log(button)
+}
 
 
+var shuffle = function (array) {
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+};
