4a68f5a4da4ddb63a49ebe740b5f11a623c5a7fc
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-19 14:08:09 -0400

    First Commit

---
+++ test.js
@@ -0,0 +1,9 @@
+// fetch(
+//   "https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/factbook.json"
+// )
+//   .then(resp => resp.json())
+//   .then(function(json) {
+//     debugger
+//     // console.log(json.countries.afghanistan.data)
+//     // console.log(Object.keys(json.countries))
+//   })
