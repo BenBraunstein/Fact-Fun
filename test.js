1e8dbbab73134f4734d316122d94c2b2affbb8a2
nicholasstano <nicholasstano@gmail.com>
2019-08-20 13:26:26 -0400

    ugh oh

--- test.js
+++ test.js
@@ -22,6 +22,7 @@ function validQuestion(randomCountry) {
         randomCountry[1].data.people == undefined ||
         randomCountry[1].data.people.population == undefined ||
         randomCountry[1].data.people.population.global_rank == undefined ||
+        randomCountry[1].data.people.religions == undefined ||
         randomCountry[1].data.people.religions.religion[0].name == undefined ||
         randomCountry[1].data.government == undefined) {
         return false
